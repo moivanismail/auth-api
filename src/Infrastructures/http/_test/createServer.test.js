a3d9e8a4a0b490780df925bc3222af5b0c062e7b
Mohammad Ismail <93242611+moivanismail@users.noreply.github.com>
2023-02-06 11:34:00 +0700

    pass say hello world test case

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -536,7 +536,7 @@ describe('HTTP server', () => {
       // Assert
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(200);
-      expect(responseJson.value).toEqual('hello world');
+      expect(responseJson.value).toEqual('Hello world!');
     });
   });
 });
